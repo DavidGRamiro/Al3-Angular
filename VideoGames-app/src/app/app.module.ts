110a62466c0b259b8c72a554682b03408b6c2179
DavidGonz <davidgramiro@gmail.com>
2022-11-27 16:42:13 +0100

    navbar done

--- VideoGames-app/src/app/app.module.ts
+++ VideoGames-app/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
