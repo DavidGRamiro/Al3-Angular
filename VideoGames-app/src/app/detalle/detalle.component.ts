d7c1e7fafe094a5c2067c8f6b9cef14dac671da1
Anni <adarvoy@gmail.com>
2022-12-03 13:33:53 +0100

    Completar Detalle

--- VideoGames-app/src/app/detalle/detalle.component.ts
+++ VideoGames-app/src/app/detalle/detalle.component.ts
@@ -1,6 +1,7 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { __importStar } from 'tslib';
 import { Juego } from '../juego';
 
 
@@ -12,22 +13,39 @@ import { Juego } from '../juego';
 export class DetalleComponent {
 
   private _idJuego: string = "";
+  public juego = new Juego();
+  constructor(route: ActivatedRoute){
+    this._idJuego = route.snapshot.params["id"];
+    console.log(this._idJuego);
 
+    let listaJuegos: { id: string, nombre: string, descripcion: string, creador: string }[] = [
+      { "id": "MNC", "nombre": "Minecraft", "descripcion": "Juego guay", "creador": "Markus Persson" },
+      { "id": "GTA", "nombre": "Grand Theft Auto", "descripcion": "Juego guay", "creador": "Markus Persson" },
+      { "id": "LOL", "nombre": "League of Legends", "descripcion": "Juego guay", "creador": "Markus Persson" },
+      { "id": "COD", "nombre": "Call of Duty", "descripcion": "Juego guay", "creador": "Markus Persson" },
+      { "id": "WOW", "nombre": "War of Warcraft", "descripcion": "Juego guay", "creador": "Markus Persson" },
+      { "id": "LOU", "nombre": "The Last of Us", "descripcion": "Juego guay", "creador": "Markus Persson" }
+      
+    ];
+
+    for(let i=0; i<listaJuegos.length; i++){
+       if ((listaJuegos[i].id) === this._idJuego){
+        this.juego.setId(listaJuegos[i].id);
+        this.juego.setNombre(listaJuegos[i].nombre);
+        this.juego.setDescripcion(listaJuegos[i].descripcion);
+        this.juego.setCreador(listaJuegos[i].creador);
+       };
+      }
+    }
+
+  // public juego = new Juego();
   // juego1: Juego = new Juego("MNC", "Minecraft", "Juego guay", "Markus Persson");
   // juego2: Juego = new Juego("WOW", "War of Warcraft", "Juego guay", "Markus Persson");
   //listaJuegos: Array<Juego> = [this.juego1 , this.juego2];
-
-  constructor(route: ActivatedRoute){
-    let listaJuegos: { id: string, name: string, descripcion: string, creador: string }[] = [
-      { "id": "", "name": "Available", "descripcion": "string", "creador": "string" },
-      { "id": "", "name": "Available", "descripcion": "string", "creador": "string" },
-      { "id": "", "name": "Available", "descripcion": "string", "creador": "string" }
-      
-  ];
-    this._idJuego = route.snapshot.params["id"];
+  // let listaJuegos1: { juego: Juego }[];
 
 
-  } 
+  
 } 
   
 
