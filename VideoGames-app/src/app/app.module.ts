2e46e8814777c2c37b5cd34193743f185c28ed29
miguel-anton <miguelantonortiz@gmail.com>
2022-12-03 21:42:47 +0100

    sigo trabajando

--- VideoGames-app/src/app/app.module.ts
+++ VideoGames-app/src/app/app.module.ts
@@ -4,11 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+<<<<<<< Updated upstream
+=======
+import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { DetalleComponent } from './detalle/detalle.component';
+import { ListadoComponent } from './listado/listado.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+
+
+
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+<<<<<<< Updated upstream
+=======
+    LoginComponent,
+    InicioComponent,
+    DetalleComponent,
+    ListadoComponent,
+    UsuarioComponent,
+    QuienesSomosComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
